c867d6a29bc66451118ec0110252d88489a5ec62
RahmatovaMaftuna <syakubov565@gmail.com>
2023-07-31 16:30:47 +0500

    JS 7-dars
    24-guruh o'quvchisi

---
+++ js/main.js
@@ -0,0 +1,77 @@
+let users = [
+  {
+    login : "MDK91",
+      password : "12345"
+  }
+];
+$(document).ready(function (){
+    $(".btn-success").click(function (){
+        let login = $("#login").val();
+        let password = $("#password").val();
+        let num = false;
+        if (login == ""){
+            alert("Login qatorni to'ldiring!");
+        }else if (password == ""){
+            alert("Parol qatorni to'ldiring")
+        }else {
+            if (users.length !=0){
+                users.forEach(function (item){
+                    if (login == item.login){
+                        if (password == item.password){
+                            num = true;
+                            alert("Siz tizimga kirdingiz!")
+                        }
+                        else{
+                            alert("Parolingiz noto'g'ri!")
+                        }
+                    }
+                });
+                if (!num){
+                    alert("Ro'yxatdan o'ting!")
+                }
+            }else{
+                alert("Siz birinchi foydalanuvchisiz.Iltimos,ro'yxatdan o'ting!")
+            }
+        }
+    })
+    $(".btn-link").click(function (){
+        let login = $("#login").val();
+        let password = $("#password").val();
+        let num = false;
+
+        if (login == ""){
+            alert("Login qatorni to'ldiring!");
+        }else if (password == ""){
+            alert("Parol qatorni to'ldiring")
+        }else{
+        if (users.length !=0){
+            users.forEach(function (item){
+                if (login == item.login){
+                    num = true
+                }
+            });
+            if (num){
+                alert("Bunday user mavjud!");
+            }else{
+                alert("Siz ro'yxatdan o'tdingiz!")
+                users.push({login: login,password: password});
+            }
+        }else{
+            users.push({login: login,password: password});
+        }
+        console.log(users);
+        }
+    })
+    $(".btn-light").on("mousedown",function (){
+        let attr = $("#password").attr("type");
+        $("#password").attr("type","text");
+        $("#fa").toggleClass("fa-eye fa-eye-slash");
+        $("#password").css("border-color","red");
+    });
+    $(".btn-light").on("mouseup",function (){
+        let attr = $("#password").attr("type");
+        $("#password").attr("type","password");
+        $(".fa").toggleClass("fa-eye fa-eye-slash")
+        $("#password").css("border-color","blue");
+    });
+});
